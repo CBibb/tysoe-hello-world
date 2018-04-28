eb5810cba09d28d82790227883f52a9f23c381e0
Christian Tysoe <chrtys0@gmail.com>
2018-04-27 17:32:11 -0700

    added in new class

--- src/edu/pcc/cis/helloworld/helloworld.java
+++ src/edu/pcc/cis/helloworld/helloworld.java
@@ -1,4 +1,14 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * A quick test
+ * Made by Christian Tysoe
+ * 2018.04.27
+ */
+
 public class helloworld {
+    public void TestClass()
+    {
+        System.out.println("test");
+    }
 }
